4361c1f5e6a051f87c9dd6b766f8eaa74ff84e6b
wr1 <8971152+wr1@users.noreply.github.com>
2025-09-23 15:39:49 +0200

    Refactor CLI to use treeparse instead of argparse

--- src/afmesh/cli/cli.py
+++ src/afmesh/cli/cli.py
@@ -1,82 +1,138 @@
-import argparse
 import logging
 import numpy as np
 from ..core.airfoil import Airfoil
 from ..utils.logger import get_logger
+from treeparse import cli, command, argument, option
 
 
-def main():
-    """Command-line interface for afmesh."""
+def plot(
+    file: str,
+    chord: float = 1.0,
+    position: list[float] = [0, 0, 0],
+    rotation: float = 0,
+    verbose: bool = False,
+):
+    """Plot an airfoil from file."""
     logger = get_logger("CLI")
-    parser = argparse.ArgumentParser(description="afmesh CLI for airfoil processing.")
-    subparsers = parser.add_subparsers(dest="command", help="Available commands")
-
-    # Plot command
-    plot_parser = subparsers.add_parser("plot", help="Plot an airfoil from file.")
-    plot_parser.add_argument(
-        "-f", "--file", required=True, help="Path to XFOIL format file."
-    )
-    plot_parser.add_argument(
-        "-c", "--chord", type=float, default=1.0, help="Chord length."
-    )
-    plot_parser.add_argument(
-        "-p",
-        "--position",
-        nargs=3,
-        type=float,
-        default=[0, 0, 0],
-        help="Position (x y z).",
-    )
-    plot_parser.add_argument(
-        "-r", "--rotation", type=float, default=0, help="Rotation in degrees."
-    )
-    plot_parser.add_argument(
-        "-v", "--verbose", action="store_true", help="Enable verbose logging."
-    )
-
-    # Remesh command
-    remesh_parser = subparsers.add_parser("remesh", help="Remesh an airfoil and save.")
-    remesh_parser.add_argument(
-        "-f", "--file", required=True, help="Path to XFOIL format file."
-    )
-    remesh_parser.add_argument(
-        "-n", "--n_points", type=int, default=100, help="Number of points per panel."
-    )
-    remesh_parser.add_argument(
-        "-o", "--output", required=True, help="Output file for remeshed points."
-    )
-    remesh_parser.add_argument(
-        "-v", "--verbose", action="store_true", help="Enable verbose logging."
-    )
-
-    args = parser.parse_args()
-
-    # Set logging level
-    if args.verbose:
+    if verbose:
         logging.getLogger().setLevel(logging.DEBUG)
         logger.info("Verbose logging enabled")
     else:
         logging.getLogger().setLevel(logging.INFO)
+    logger.info(f"Plotting airfoil from {file}")
+    af = Airfoil.from_xfoil(
+        file,
+        chord=chord,
+        position=tuple(position),
+        rotation=rotation,
+    )
+    af.plot()
+    logger.debug("Plot command completed")
 
-    if args.command == "plot":
-        logger.info(f"Plotting airfoil from {args.file}")
-        af = Airfoil.from_xfoil(
-            args.file,
-            chord=args.chord,
-            position=tuple(args.position),
-            rotation=args.rotation,
-        )
-        af.plot()
-        logger.debug("Plot command completed")
-    elif args.command == "remesh":
-        logger.info(f"Remeshing airfoil from {args.file}")
-        af = Airfoil.from_xfoil(args.file)
-        af.remesh(n_points=args.n_points)
-        np.savetxt(args.output, af.current_points[:, :2], header="x y", comments="")
-        logger.info(f"Remeshed points saved to {args.output}")
-        logger.debug("Remesh command completed")
+
+def remesh(
+    file: str,
+    n_points: int = 100,
+    output: str,
+    verbose: bool = False,
+):
+    """Remesh an airfoil and save."""
+    logger = get_logger("CLI")
+    if verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+        logger.info("Verbose logging enabled")
     else:
-        parser.print_help()
+        logging.getLogger().setLevel(logging.INFO)
+    logger.info(f"Remeshing airfoil from {file}")
+    af = Airfoil.from_xfoil(file)
+    af.remesh(n_points=n_points)
+    np.savetxt(output, af.current_points[:, :2], header="x y", comments="")
+    logger.info(f"Remeshed points saved to {output}")
+    logger.debug("Remesh command completed")
+
+
+app = cli(
+    name="afmesh",
+    help="Command-line interface for afmesh airfoil processing.",
+    max_width=120,
+    show_types=True,
+    show_defaults=True,
+    line_connect=True,
+    theme="monochrome",
+)
+
+plot_cmd = command(
+    name="plot",
+    help="Plot an airfoil from file.",
+    callback=plot,
+    arguments=[
+        argument(name="file", arg_type=str, help="Path to XFOIL format file.", sort_key=0),
+    ],
+    options=[
+        option(
+            flags=["--chord", "-c"],
+            arg_type=float,
+            default=1.0,
+            help="Chord length.",
+            sort_key=0,
+        ),
+        option(
+            flags=["--position", "-p"],
+            arg_type=float,
+            nargs=3,
+            default=[0, 0, 0],
+            help="Position (x y z).",
+            sort_key=1,
+        ),
+        option(
+            flags=["--rotation", "-r"],
+            arg_type=float,
+            default=0,
+            help="Rotation in degrees.",
+            sort_key=2,
+        ),
+        option(
+            flags=["--verbose", "-v"],
+            arg_type=bool,
+            default=False,
+            help="Enable verbose logging.",
+            sort_key=3,
+        ),
+    ],
+)
+app.commands.append(plot_cmd)
+
+remesh_cmd = command(
+    name="remesh",
+    help="Remesh an airfoil and save.",
+    callback=remesh,
+    arguments=[
+        argument(name="file", arg_type=str, help="Path to XFOIL format file.", sort_key=0),
+        argument(name="output", arg_type=str, help="Output file for remeshed points.", sort_key=1),
+    ],
+    options=[
+        option(
+            flags=["--n_points", "-n"],
+            arg_type=int,
+            default=100,
+            help="Number of points per panel.",
+            sort_key=0,
+        ),
+        option(
+            flags=["--verbose", "-v"],
+            arg_type=bool,
+            default=False,
+            help="Enable verbose logging.",
+            sort_key=1,
+        ),
+    ],
+)
+app.commands.append(remesh_cmd)
+
+
+def main():
+    """Entry point for the CLI."""
+    app.run()
 
 
 if __name__ == "__main__":
